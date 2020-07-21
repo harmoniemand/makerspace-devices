b45f38eabf732725a77ea7f1faa2b60eef1a85f9
Jonathan Günz <jonathan.guenz@experimenta.science>
2020-07-21 21:52:56 +0200

    some mobile fixes

--- src/Reservation/partials/reservation-pos-wp.partial.php
+++ src/Reservation/partials/reservation-pos-wp.partial.php
@@ -134,21 +134,27 @@
 
                     ?>
 
-                    <tr id="post-3222" class="iedit author-self level-0 post-3222 type-page status-publish hentry">
+                    <tr class="iedit author-self level-0 type-page status-publish hentry">
 
-                        <td class="title column-title has-row-actions column-primary page-title" data-colname="Titel">
-                            <div class="locked-info"><span class="locked-avatar"></span> <span class="locked-text"></span></div>
-                            <?php echo $r_user->user_firstname ?>
+                        <td class="title column-title has-row-actions column-primary page-title" data-colname="Vorname">
+                            <span><?php echo $r_user->user_firstname ?></span>
+                            <span class="hidden"><?php echo $r_user->user_lastname ?></span>
 
-                            <!-- <div class="row-actions"><span class="edit"><a href="/wp-admin/post.php?post=3222&amp;action=edit" aria-label="„Aktuell befinden sich“ bearbeiten">Bearbeiten</a> | </span><span class="inline hide-if-no-js"><button type="button" class="button-link editinline" aria-label="Schnellanpassung für „Aktuell befinden sich“ (inline)" aria-expanded="false">QuickEdit</button> | </span><span class="trash"><a href="/wp-admin/post.php?post=3222&amp;action=trash&amp;_wpnonce=378bf7b306" class="submitdelete" aria-label="„Aktuell befinden sich“ in den Papierkorb verschieben">Papierkorb</a> | </span><span class="view"><a href="/ds-corona-anwesende/" rel="bookmark" aria-label="„Aktuell befinden sich“ ansehen">Anschauen</a></span></div><button type="button" class="toggle-row"><span class="screen-reader-text">Mehr Details anzeigen</span></button> -->
+                            <div class="hidden" id="inline_3240">
+                                <div class="post_title"><?php echo $r_user->user_lastname  ?></div>
+                                <div class="post_name"><?php echo $r_user->user_login  ?></div>
+                            </div>
+
+                            <button type="button" class="toggle-row"><span class="screen-reader-text">Mehr Details anzeigen</span></button>
                         </td>
-                        <td class="title column-title has-row-actions column-primary page-title" data-colname="Titel">
+
+                        <td class="" data-colname="Nachname">
                             <div class="locked-info"><span class="locked-avatar"></span> <span class="locked-text"></span></div>
                             <?php echo $r_user->user_lastname  ?>
 
                             <!-- <div class="row-actions"><span class="edit"><a href="/wp-admin/post.php?post=3222&amp;action=edit" aria-label="„Aktuell befinden sich“ bearbeiten">Bearbeiten</a> | </span><span class="inline hide-if-no-js"><button type="button" class="button-link editinline" aria-label="Schnellanpassung für „Aktuell befinden sich“ (inline)" aria-expanded="false">QuickEdit</button> | </span><span class="trash"><a href="/wp-admin/post.php?post=3222&amp;action=trash&amp;_wpnonce=378bf7b306" class="submitdelete" aria-label="„Aktuell befinden sich“ in den Papierkorb verschieben">Papierkorb</a> | </span><span class="view"><a href="/ds-corona-anwesende/" rel="bookmark" aria-label="„Aktuell befinden sich“ ansehen">Anschauen</a></span></div><button type="button" class="toggle-row"><span class="screen-reader-text">Mehr Details anzeigen</span></button> -->
                         </td>
-                        <td class="title column-title has-row-actions column-primary page-title" data-colname="Titel">
+                        <td class="" data-colname="Username">
                             <div class="locked-info"><span class="locked-avatar"></span> <span class="locked-text"></span></div>
                             <strong>
                                 <a class="row-title" href="/wp-admin/user-edit.php?user_id=<?php echo $r->mar_user_id ?>" aria-label="„Aktuell befinden sich“ (Bearbeiten)">
@@ -156,13 +162,13 @@
                                 </a>
                             </strong>
                         </td>
-                        <td class="author column-author" data-colname="Autor">
+                        <td class="" data-colname="Von - Bis">
                             <span class=""><?php echo $r_from->format('H:i') ?></span>
                             -
                             <span class=""><?php echo $r_to->format('H:i') ?></span>
                         </td>
 
-                        <td class="comments column-comments" data-colname="Kommentare">
+                        <td class="" data-colname="SUs">
                             <span class="">
                                 <?php if (get_user_meta($r->mar_user_id, 'ms_user_corona_safetyinstruction', false)) : ?>
                                     <button type="submit" class="btn btn-link btn-sm" id="ms_user_corona_safetyinstruction" name="ms_user_corona_safetyinstruction" value="<?php echo $r->mar_user_id ?>">
@@ -176,7 +182,7 @@
                             </span>
                         </td>
 
-                        <td class="comments column-comments" data-colname="Kommentare">
+                        <td class="" data-colname="Anschrift">
                             <span class="">
                                 <?php if (get_user_meta($r->mar_user_id, 'ms_user_corona_adress', false)) : ?>
                                     <button type="submit" class="btn btn-link btn-sm" id="ms_user_corona_adress" name="ms_user_corona_adress" value="<?php echo $r->mar_user_id ?>">
@@ -190,14 +196,14 @@
                             </span>
                         </td>
 
-                        <td class="date column-date" data-colname="Datum">
+                        <td class="" data-colname="Aktionen">
                             <?php if (count($mpl_entries) % 2 == 0) : ?>
-                                <button type="submit" class="btn btn-outline-dark btn-sm w-100" id="mp_create_log" name="mp_create_log" value="<?php echo $r->mar_user_id ?>" <?php echo $disable_create_log ?>>
+                                <button type="submit" class="btn btn-link-dark btn-sm" id="mp_create_log" name="mp_create_log" value="<?php echo $r->mar_user_id ?>" <?php echo $disable_create_log ?>>
                                     <clr-icon shape="login"></clr-icon>
                                     kommen
                                 </button>
                             <?php else : ?>
-                                <button type="submit" class="btn btn-outline-dark btn-sm w-100" id="mp_create_log" name="mp_create_log" value="<?php echo $r->mar_user_id ?>" <?php echo $disable_create_log ?>>
+                                <button type="submit" class="btn btn-link-dark btn-sm" id="mp_create_log" name="mp_create_log" value="<?php echo $r->mar_user_id ?>" <?php echo $disable_create_log ?>>
                                     <clr-icon shape="logout"></clr-icon>
                                     gehen
                                 </button>
